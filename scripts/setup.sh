ee48f523071e9e950e9a50af474d8624d1ce38fb
Igal Sapir <isapir@apache.org>
2023-07-08 21:07:34 -0700

    Small changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,5 @@
-## set the user and group ownership of mnt to 1000:1000, and the setgid flag for mnt
+## set the user and group ownership of mnt to current user:group, e.g. 1000:1000, and the setgid flag for mnt
 
-# sudo chown -R "$(id -u):$(id -g)" ../..
+sudo chown -R "$(id -u):$(id -g)" ../..
 
-sudo chown -R "1000:1000" ../..
-
-sudo chmod -R u=rwsX,g=rwsX,o=rw ../mnt
+sudo chmod -R 2775 ../mnt
