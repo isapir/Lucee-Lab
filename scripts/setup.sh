4df8577ddd41e2623d371a2b7db175a7e3253249
Igal Sapir <isapir@apache.org>
2023-10-28 20:08:10 -0700

    Clarified setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,8 @@
 ## set the user and group ownership of mnt to current user:group, e.g. 1000:1000, and the setgid flag for mnt
+## cd into this directory and run this script
 
 sudo chown -R "$(id -u):$(id -g)" ..
 
 sudo chmod -R 2775 ../mnt
+
+ls -aln ../mnt
